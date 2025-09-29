ccee9058d92822fed2b68bdf90617dcc9ad171c8
Helen Brazier <helenbrazier@Helens-MacBook-Air.local>
2025-09-29 07:51:14 -0500

    fix: correct Vite base and rebuild docs

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CourseProvider>
+    <HashRouter>                      
+      <CourseProvider>                  
         <App />
       </CourseProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
