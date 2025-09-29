3560697d634efe6c787e302c20151589d0a454e7
Helen Brazier <helenbrazier@Helens-MacBook-Air.local>
2025-09-29 12:35:30 -0500

    fix: import HashRouter and rebuild docs

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";   // <-- change this
 import App from "./App.jsx";
-import CourseProvider from "./state/CourseProvider.jsx"; 
+import CourseProvider from "./state/CourseProvider.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>                      
-      <CourseProvider>                  
+    <HashRouter>
+      <CourseProvider>
         <App />
       </CourseProvider>
     </HashRouter>
